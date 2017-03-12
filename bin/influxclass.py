cbb7ac4e265b72cf7078d6a4d8d3ad4f3c4334ff
ramdog69 <cramsay@vistaprint.com>
2017-03-12 23:30:03 +0000

    Added total Interest

--- bin/influxclass.py
+++ bin/influxclass.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from influxdb import InfluxDBClient
+import json
 
 class influxClass(object):
    def __init__(self, confdata):
@@ -13,7 +14,8 @@ class influxClass(object):
 
    def writeToDB(self, json_body):
       self.client.write_points(json_body)
-      
-   def getInterest(self):
-      return self.client.query('select receivedInterest from availablecash;')
 
+   def getInterest(self):
+      interest_json=self.client.query('select last(receivedInterest) from availablecash;')
+      #Generate a list from the datapoints of the returned result set and return the dictionatry item 'last'
+      return list(interest_json.get_points())[0].get('last')
